3c5e63e8ccacc25836d25b7bc87ce3e25692f6f9
Vivi Chung <vivi31527@hotmail.com.tw>
2021-11-19 16:59:06 +0800

    更新成使用者自訂數字範圍

--- guessno.py
+++ guessno.py
@@ -1,16 +1,20 @@
 import random
+start = input('請決定隨機數字範圍最小值(min):')
+end = input('請決定隨機數字範圍最大值(max):')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0
 while True:
 	num = input('請猜一個數字:')
 	num = int(num)
+	count += 1
 	if num == r:
-		print('恭喜你猜對了')
+		print('這是你猜的第', count, '次, 恭喜你猜對了!')
 		break
 	elif num > r:
 		print('猜錯了~答案是更小的數字!')
 	elif num < r:
 		print('猜錯了~答案是更大的數字!')
-	count += 1
 	print('這是你猜的第', count, '次')
